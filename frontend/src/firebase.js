3dac040fa08ab2cabfb956175598f68115b2bd79
Aviral000 <avimalik02@gmail.com>
2024-07-22 12:33:45 +0530

    changed

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,24 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCNPQTvSupeCuHXaPfhYQhhP0pw2U3elCQ",
-  authDomain: "task-reminder-5da7a.firebaseapp.com",
-  projectId: "task-reminder-5da7a",
-  storageBucket: "task-reminder-5da7a.appspot.com",
-  messagingSenderId: "144663252291",
-  appId: "1:144663252291:web:1b4e63c25ab8f3121d73f6",
-  measurementId: "G-02R6LGC8H4"
+  apiKey: "AIzaSyBjpD8Co1bp5yc5a_d5nmKkmRv5sOzlsyo",
+  authDomain: "aws-polaris.firebaseapp.com",
+  projectId: "aws-polaris",
+  storageBucket: "aws-polaris.appspot.com",
+  messagingSenderId: "226462897577",
+  appId: "1:226462897577:web:020042db72cf68b57f60b8"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export default app;
